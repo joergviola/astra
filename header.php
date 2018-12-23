930011960f80019b6e8b37e32ca6dc07a9c7bcd9
joergviola <viola@objectcode.de>
2018-12-23 08:14:39 +0100

    Analytics und Site Verification

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
+<meta name="google-site-verification" content="" />
 
 <?php astra_head_bottom(); ?>
 <?php wp_head(); ?>
