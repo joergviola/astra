1e000f89cb796aa4cdd6aecc476665c4a9e18ac1
joergviola <joerg@joergviola.de>
2019-03-26 11:18:30 +0100

    Farben

--- custom/index.php
+++ custom/index.php
@@ -5,25 +5,40 @@
 
 $my_color_palette = array(
 	array(
-		'name'  => __( 'navy blue', 'themeLangDomain' ),
-		'slug'  => 'navy-blue',
-		'color' => '#0f1626',
+		'name'  => __( 'black', 'themeLangDomain' ),
+		'slug'  => 'black',
+		'color' => '#000000',
 	),
 	array(
-		'name'  => __( 'leather', 'themeLangDomain' ),
-		'slug'  => 'leather',
-		'color' => '#ab987a',
+		'name'  => __( 'white', 'themeLangDomain' ),
+		'slug'  => 'white',
+		'color' => '#ffffff',
 	),
 	array(
-		'name'  => __( 'coral', 'themeLangDomain' ),
-		'slug'  => 'coral',
-		'color' => '#ff533d',
+		'name'  => __( 'blue', 'themeLangDomain' ),
+		'slug'  => 'blue',
+		'color' => '#7777ff',
 	),
-	array(
-		'name'  => __( 'eggshell', 'themeLangDomain' ),
-		'slug'  => 'eggshell',
-		'color' => '#f5f5f5',
-	),
+//	array(
+//		'name'  => __( 'navy blue', 'themeLangDomain' ),
+//		'slug'  => 'navy-blue',
+//		'color' => '#0f1626',
+//	),
+//	array(
+//		'name'  => __( 'leather', 'themeLangDomain' ),
+//		'slug'  => 'leather',
+//		'color' => '#ab987a',
+//	),
+//	array(
+//		'name'  => __( 'coral', 'themeLangDomain' ),
+//		'slug'  => 'coral',
+//		'color' => '#ff533d',
+//	),
+//	array(
+//		'name'  => __( 'eggshell', 'themeLangDomain' ),
+//		'slug'  => 'eggshell',
+//		'color' => '#f5f5f5',
+//	),
 //	array(
 //		'name'  => __( 'very dark gray', 'themeLangDomain' ),
 //		'slug'  => 'very-dark-gray',
@@ -42,10 +57,10 @@ function your_prefix_astra_color_palettes() {
 	global $my_color_palette;
 
 	$color_palettes = array(
-		'#a156b4',
-		'#d0a5db',
-		'#eee',
-		'#444',
+		'#000000',
+		'#ffffff',
+		'#7777ff',
+		'#000000',
 		'#000000',
 		'#000000',
 		'#000000',
